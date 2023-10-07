4e63bae917e0f9336a049bace026443f166dd9a7
Kelvin okumba <okumbakelvin@gmail.com>
2023-10-07 11:42:05 +0300

    completed

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -4,6 +4,7 @@ def return_evens(num_list):
     return [item for item in num_list if item % 2 == 0]
 
 
+
 def make_exclamation(sentence_list):
     return [sentence + '!' for sentence in sentence_list]
 
