1f0a89ca27123c264161020b9e365c35719f45c5
Kelvin okumba <okumbakelvin@gmail.com>
2023-10-07 11:34:02 +0300

    added

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -6,3 +6,7 @@ def return_evens(num_list):
 
 def make_exclamation(sentence_list):
     return [sentence + '!' for sentence in sentence_list]
+
+sentence_list=["Hello", "I'm doing great", "Python is fun"]
+result=make_exclamation(sentence_list)
+print(result)
